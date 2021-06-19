4d408b4e9710ac2ba4166e210b17e0cfbb7eb620
Derek Phelps <derekphtpt@gmail.com>
2021-06-18 23:03:12 -0600

    added welcome modal and food genre check boxes

---
+++ script.js
@@ -0,0 +1,7 @@
+// const welcomeModalButton = document.querySelector("welcomeModal");
+// const modalBg = document.querySelector(".modal-background");
+// const modal = document.querySelector(".modal");
+
+// welcomeModalButton.addEventListener("click", () => {
+//   modal.classList.add("is-active");
+// });

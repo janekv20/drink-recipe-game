df2f22fa6a3bc5631e6009a77f3d6ca2a7edee3f
James Bowen (J.D.) <j.bowen1821@gmail.com>
2021-06-22 19:44:38 -0500

    fetch api updated

--- assets/js/result.js
+++ assets/js/result.js
@@ -38,38 +38,67 @@ var foodInput = document.querySelector(".food");
 var locationInput = document.querySelector(".location");
 var foodInputEl = foodInput.value;
 var locationInputEl = locationInput.value;
+var resultsDisplay = document.querySelector(".results")
 
 const api_key = 'bX-trm4auNudxiiAeCbSA6jNygnErRfxAHTFg6uFaE_muyIDm9VDO6s4cZyx7M-2N7prlSry13TwEvrwzpq3OkO83KVAdcLXRMquZZ8zXK7WxyrgKHs-sk0RIjnRYHYx';
 
 // create AJAX call
-function displayRestaurants() {
+function displayRestaurants(data) {
 
     var queryURL = "https://api.yelp.com/v3/businesses/search?" + foodInputEl + "=&location=" + locationInputEl
 
-    $.ajax({
-        url: queryURL,
-        headers: {
-            'Authorization': 'Bearer ' + api_key
-        },
-        method: "GET",
-        dataType: 'json',
-        success: function () {
-            //Display a header on the page with results
-            $('#results').append(' <h5>Here are your restaurant options!</h5>');
-            //Iterate through the JSON array of restaurants which was returned by Yelp API
-            $.each(data.businesses, function (i, item) {
-                //Store each restaurant object in a variable
+    
+        fetch(queryURL)
+          .then(function(response) {
+            // request was successful
+            if (response.ok) {
+            //  console.log(response);
+               return response.json();
+            } else {
+              alert("Error: " + response.statusText);
+            }
+          })
+            .then((data) => {
                 var restaurantName = item.name;
                 var foodGenre = item.categories;
                 var yelpRating = item.rating;
                 var restaurantLoc = item.location;
                 var cost = item.price;
-                //Append results onto homepage
-                $('results').append('Restaurant: <b>' + restaurantName + '<b><br> Food Genre: ' + foodGenre + '<br>Rating: ' + yelpRating + '<br>Location: ' + restaurantLoc + '<br>Price: ' + cost);
-            });
-        }
+             
+                resultsDisplay.textContent =('Restaurant: <b>' + restaurantName + '<b><br> Food Genre: ' + foodGenre + '<br> Rating: ' + yelpRating + '<br> Location: ' + restaurantLoc + '<br> Price: ' + cost);
     }
+            )}
 
-    )
-}
+    
+
+
+
+
+
+//     $.ajax({
+//         url: queryURL,
+//         headers: {
+//             'Authorization': 'Bearer ' + api_key
+//         },
+//         method: "GET",
+//         dataType: 'json',
+//         success: function () {
+//             //Display a header on the page with results
+//             $('#results').append(' <h5>Here are your restaurant options!</h5>');
+//             //Iterate through the JSON array of restaurants which was returned by Yelp API
+//             $.each(data.businesses, function (i, item) {
+//                 //Store each restaurant object in a variable
+//                 var restaurantName = item.name;
+//                 var foodGenre = item.categories;
+//                 var yelpRating = item.rating;
+//                 var restaurantLoc = item.location;
+//                 var cost = item.price;
+//                 //Append results onto homepage
+//                 $('results').append('Restaurant: <b>' + restaurantName + '<b><br> Food Genre: ' + foodGenre + '<br>Rating: ' + yelpRating + '<br>Location: ' + restaurantLoc + '<br>Price: ' + cost);
+//             });
+//         }
+//     }
+
+//     )
+// }
 
